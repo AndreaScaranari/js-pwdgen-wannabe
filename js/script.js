0ad678d33c3282b513affece4eca112b10875c7f
AndreaScaranari <andrea.scaranari.93@gmail.com>
2023-12-15 15:48:59 +0100

    firstName_lastName_favColor_created

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log ("Hakuna Matata")
+// console.log ("Hakuna Matata")
+
+// Chiesto il nome all'utente
+const firstName = prompt("Qual è il tuo nome?", "Silvio");
+console.log(firstName);
+
+// Chiesto il cognome all'utente
+const secondName = prompt("Qual è il tuo cognome?", "Pellico");
+console.log(secondName);
+
+// Chiesto il colore preferito dell'utente
+const favColor = prompt("Qual è il tuo colore preferito?", "Viola");
+console.log(favColor);
